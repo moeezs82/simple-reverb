d54909e17324b44ebd619a8d0cc24451281c40b6
moeezs82 <mora03033@gmail.com>
2024-04-11 16:08:58 +0500

    chat app completed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/message-sent', function(Request $request){
+    App\Events\MessageSent::dispatch('Guest', $request->message);
+
+    return response()->json('done');
 });
